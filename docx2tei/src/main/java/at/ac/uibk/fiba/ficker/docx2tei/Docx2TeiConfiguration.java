37c520842dc6c4a7090299d12b1c7d4c4a342de9
joseph <joseph.wang@uibk.ac.at>
2024-05-08 21:31:59 +0200

    Removed docx2tei routines, not needed in the webapp

--- docx2tei/src/main/java/at/ac/uibk/fiba/ficker/docx2tei/Docx2TeiConfiguration.java
+++
@@ -1,9 +0,0 @@
-package at.ac.uibk.fiba.ficker.docx2tei;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class Docx2TeiConfiguration {
-}
--- docx2tei/src/test/java/at/ac/uibk/fiba/ficker/docx2tei/TestBase.java
+++
@@ -1,9 +0,0 @@
-package at.ac.uibk.fiba.ficker.docx2tei;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-
-@SpringBootTest
-@ContextConfiguration(classes = {Docx2TeiConfiguration.class})
-public abstract class TestBase {
-}
